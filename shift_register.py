91e97268b1bab4c955e75e8cfc1ca17054aa49e2
Reese Wilson <3253971+figadore@users.noreply.github.com>
2020-07-10 06:14:08 +0100

    wip shift registers

---
+++ shift_register.py
@@ -0,0 +1,95 @@
+import time
+
+from collections import OrderedDict
+from gpiozero import CompositeDevice, SourceMixin, GPIOPinMissing, OutputDevice
+from gpiozero import Button
+
+
+class ShiftRegister(SourceMixin, CompositeDevice):
+    def __init__(self, data=None, latch=None, clock=None, initial_value=None, bit_count=8, pin_factory=None):
+        if not all(p is not None for p in [data, latch, clock]):
+            raise GPIOPinMissing('data, latch, and clock pins must be provided')
+        devices = OrderedDict((
+            ('data_device', OutputDevice(data, pin_factory=pin_factory)),
+            ('latch_device', OutputDevice(latch, initial_value=True, pin_factory=pin_factory)),
+            ('clock_device', OutputDevice(clock, pin_factory=pin_factory)),
+        ))
+        self._bit_count = bit_count
+        super().__init__(_order=devices.keys(), **devices)
+
+        if initial_value is None:
+            initial_value = int('0' * self.bit_count, 2)
+        self.value = initial_value
+
+    @property
+    def bit_count(self):
+        return self._bit_count
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._shift_out(value)
+        self._value = value
+
+    def on(self, bit):
+        self.value |= 1 << bit
+
+    def off(self, bit):
+        self.value &= ~(1 << bit)
+
+    def toggle(self, bit):
+        self.value ^= 1 << bit
+
+    def _shift_out(self, value):
+        self.latch_device.off()
+        for x in range(self.bit_count):
+            self.data_device.value = (value >> x) & 1
+            self.clock_device.off()
+            self.clock_device.on()
+            self.clock_device.off()
+        self.latch_device.on()
+        self.latch_device.off()
+        self.data_device.off()
+
+register = ShiftRegister(data=4, latch=6, clock=5)
+black = Button(26)
+red = Button(19)
+while True:
+    if black.is_pressed:
+        register.on(7)
+        register.off(6)
+    else:
+        register.off(7)
+        register.on(6)
+    if red.is_pressed:
+        register.off(5)
+        register.on(4)
+    else:
+        register.on(5)
+        register.off(4)
+    time.sleep(.2)
+
+#register = ShiftRegister(data=4, latch=6, clock=5)
+##register.on(7)
+#print(register.value)
+#time.sleep(2)
+#register.on(6)
+#print(register.value)
+#time.sleep(1)
+#register.on(5)
+#print(register.value)
+#time.sleep(1)
+#register.on(4)
+#print(register.value)
+#time.sleep(1)
+#register.on(3)
+#print(register.value)
+#time.sleep(1)
+#register.off(5)
+#print(register.value)
+#time.sleep(1)
+#register.value = 0
+#time.sleep(5)
---
+++ shift_register_sandbox.py
@@ -0,0 +1,83 @@
+import gpiozero
+import argparse
+import time
+
+shift_pin = gpiozero.OutputDevice(5)
+reset_pin = gpiozero.OutputDevice(9)
+store_pin = gpiozero.OutputDevice(6)
+data_pin = gpiozero.OutputDevice(4)
+output_pin = gpiozero.OutputDevice(10)
+
+def reset():
+    reset_pin.off()
+    time.sleep(.1)
+    reset_pin.on()
+
+def shift():
+    shift_pin.off()
+    time.sleep(.1)
+    shift_pin.on()
+
+def store():
+    store_pin.off()
+    time.sleep(.1)
+    store_pin.on()
+
+def toggle_output():
+    output_pin.toggle()
+
+def toggle_data():
+    data_pin.toggle()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    output_pin.off() #enable output on low
+    reset_pin.on() #resets on low
+
+    parser.add_argument('--reset', action='store_true')
+    parser.add_argument('--shift', action='store_true')
+    parser.add_argument('--store', action='store_true')
+    parser.add_argument('--toggle-output', action='store_true')
+    parser.add_argument('--toggle-data', action='store_true')
+    args = parser.parse_args()
+
+    if args.reset:
+        reset()
+
+    if args.shift:
+        shift()
+
+    if args.store:
+        store()
+
+    if args.toggle_output:
+        toggle_output()
+
+    if args.toggle_data:
+        toggle_data()
+
+    reset()
+    toggle_data()
+    shift()
+    store()
+    toggle_data()
+    time.sleep(1)
+    #toggle_data()
+    shift()
+    store()
+    time.sleep(1)
+    #toggle_data()
+    shift()
+    store()
+    #toggle_output()
+    time.sleep(1)
+    #toggle_data()
+    shift()
+    store()
+    time.sleep(1)
+    #toggle_data()
+    shift()
+    store()
+    #toggle_output()
+    time.sleep(1)
